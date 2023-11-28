9be42f09254170ad854bb06cf8b2ffd3e4fbce90
muzammil-raaj-raxa <muzammilraj0001@gmail.com>
2023-11-28 21:19:15 +0500

    Generate Random key and store it on firebase

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const moment = require('moment-timezone');
+const generateRandomKey = require('./generateRandomKey');
+
+const app = express();
+const port = 3001;
+
+// Initialize Firebase Admin SDK with your credentials
+const serviceAccount = require('./firebase-credentials.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Generate and store a random key in Firestore
+app.get('/randomKey', async (req, res) => {
+  try {
+    // Generate a random key using the imported function
+    const randomKey = generateRandomKey(7);
+
+    // Initialize Firestore
+    const firestore = admin.firestore();
+
+    // Generate a unique document ID using a timestamp in the 'Asia/Karachi' timezone
+    const timestamp = moment().tz('Asia/Karachi').format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+    const documentId = `key_${timestamp}`;
+
+    // Store the random key in Firestore
+    const keyRef = firestore.collection('keys').doc(documentId);
+    await keyRef.set({ value: randomKey });
+
+    res.json({ message: 'key generated and stored' });
+    console.log('Key Generated')
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ generateRandomKey.js
@@ -0,0 +1,14 @@
+
+function generateRandomKey(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomKey = '';
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomKey += characters.charAt(randomIndex);
+    }
+
+    return randomKey;
+  }
+
+  module.exports = generateRandomKey;
